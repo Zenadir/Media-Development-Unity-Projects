22459ca0d16d771c3656223edd5d1220bb846369
Gabe Dworman <31422364+Zenadir@users.noreply.github.com>
2017-10-04 22:31:26 -0400

    Vehicle and Collision Detection

---
+++ Dworman_CollisionDetection/CheckCollision.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour {
+
+    //Determnes how collision should be checked
+    enum collisionMode { AABB, Circle};
+
+    //By default the collision checking mode is set to AABB
+    collisionMode currentMode = collisionMode.AABB;
+
+    //A list holding all of the gameobject planets and their renderers.
+    List<GameObject> planets;
+    List<SpriteRenderer> planetRenderers;
+
+    public GameObject player;
+    SpriteRenderer playerRenderer;
+
+    float playerRadius;
+    float planetRadius;
+
+   
+     
+	void Start () { 
+
+       
+        //Get all of the planets in the screen and store them
+        planets = new List<GameObject>();
+        planetRenderers = new List<SpriteRenderer>();
+        planets.AddRange(GameObject.FindGameObjectsWithTag("Planet"));
+        
+       //Store the sprite renderer of each of the stored planets
+        foreach (GameObject planet in planets)
+        {
+            planetRenderers.Add(planet.GetComponent<SpriteRenderer>());
+        }
+
+        //Store the player in the scene
+        player = GameObject.FindGameObjectWithTag("player");
+        playerRenderer = player.GetComponent<SpriteRenderer>();
+
+        planetRadius = planetRenderers[0].bounds.extents.x;
+        playerRadius = (playerRenderer.bounds.extents.x + playerRenderer.bounds.extents.y)/2;
+     
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        //If the ship's color isnt white, set it to white by default
+        if(playerRenderer.color!= Color.white)
+        {
+            playerRenderer.color = Color.white;
+        }
+      
+        //Give the opportunity for the user to switch collision modes
+        ChangeCollision();
+
+        //Check for collision of the apropriate collision type
+        //depending on the current mode
+        if (currentMode == collisionMode.AABB)
+        {
+            AABB_Collision();
+        }
+
+        if( currentMode == collisionMode.Circle)
+        {
+            CircleCollision();
+        }
+    }
+
+    void ChangeCollision()
+    {
+        playerRenderer.color = Color.white;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentMode = collisionMode.AABB;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentMode = collisionMode.Circle;
+        }
+    }
+
+    //Check collision based on the rectangular sprites around planets and the player
+    void AABB_Collision()
+    {
+        //For each planet, check to see if the rectangular bounds of the sprite containing the 
+        //asteroid intersect the bounds of the player's sprite.
+        foreach (SpriteRenderer planetRenderer in planetRenderers)
+        {
+            //Reset the color of the planet
+            if(planetRenderer.color != Color.white)
+            {
+                planetRenderer.color = Color.white;
+            }
+
+           
+            if (playerRenderer.bounds.min.x < planetRenderer.bounds.max.x)
+            {
+                if (playerRenderer.bounds.max.x > planetRenderer.bounds.min.x)
+                {
+                    if (playerRenderer.bounds.min.y < planetRenderer.bounds.max.y)
+                    {
+                        if (playerRenderer.bounds.max.y > planetRenderer.bounds.min.y)
+                        {
+                            //If the planet intersects the player, change both of their colors to red. 
+                            planetRenderer.color = Color.red;
+                            playerRenderer.color = Color.red;
+                        }
+
+                    }
+               
+
+                }
+
+            }
+
+        }
+    }
+    
+    //foreach asteroid, if the radius of that planet intersects the radius of the player, turn both of them blue
+    void CircleCollision()
+    {
+        foreach (SpriteRenderer renderer in planetRenderers)
+        {
+            if ((playerRenderer.bounds.center - renderer.bounds.center).magnitude < (playerRadius + planetRadius) )
+            {
+                playerRenderer.color = Color.blue;
+                renderer.color = Color.blue;
+        
+            }
+            else
+            {
+                renderer.color = Color.white;
+            }
+        }
+    }
+    
+}
---
+++ Dworman_CollisionDetection/Vehicle.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Vehicle class
+// Placed on the Tank prefab
+// Move a vehicle around the screen
+
+public class Vehicle : MonoBehaviour 
+{
+
+    // Fields
+    //[SerializeField]					// private field but shows in the inspector
+
+    private Vector3 vehiclePosition = Vector3.zero; // position of the vehicle in a given frame
+
+    public Vector3 direction = Vector3.up;           // way the object is facing
+
+    public Vector3 velocity = Vector3.zero;
+
+    Vector3 acceleration = Vector3.zero;
+
+    float accelerationRate = 0.003f; 
+
+    float decceleration = .95f;
+
+
+    float maxSpeed = 1f;
+    // Use this for initialization
+
+
+    public Camera mainCamera;
+	void Start () 
+	{
+		// Capture the vehicle's starting position
+	
+		vehiclePosition = transform.position;
+
+   
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            //Calculate the vehicles acceleration vector
+            acceleration = direction * accelerationRate;
+
+            //Add the acceleration to the velocity
+            velocity += acceleration;
+        }
+        //If the user isnt holding the buttton, cause the vehicle to deccelerate
+        else
+        {
+            velocity *= decceleration;
+        }
+      
+
+
+
+        //Cap off the velocity so that the vehicle can't move too fast
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        //Move the vehicle using the velocity vector.
+        vehiclePosition += velocity;
+
+
+
+
+        //Set the position.
+        transform.position = vehiclePosition;
+
+        screenWrap();
+
+    }
+
+    //Check to see if the vehicle is outside of the camera's range. If it is, invert its position across the origin.
+    void screenWrap()
+    {
+
+        if (transform.position.x > mainCamera.orthographicSize*(mainCamera.aspect)|| transform.position.x < -(mainCamera.orthographicSize * ( mainCamera.aspect)))
+        {
+            vehiclePosition = new Vector3(-transform.position.x, transform.position.y, 0);
+            transform.position = new Vector3(-transform.position.x, transform.position.y, 0);
+        }
+
+        if (transform.position.y > mainCamera.orthographicSize || transform.position.y < -(mainCamera.orthographicSize))
+        {
+            vehiclePosition = new Vector3(transform.position.x, -transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x, -transform.position.y, 0);
+        }
+
+    }
+
+}
+
+
+
+
